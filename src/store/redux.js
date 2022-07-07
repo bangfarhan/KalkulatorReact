991f21ebda8582f72f2887642815a596f2d2e82e
bangfarhan <farhanbeunghar@gmail.com>
2022-07-07 22:32:18 +0700

    update 2

--- src/store/redux.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import kalkulator from "../kalkulator/kalkulator";
-const store = configureStore({
-  reducer: { kalkulator},
-});
-export default store;
